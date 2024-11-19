5ce6d0e111be70df9ef96c2fae6a427cc7af86d7
ksennija <ksenia.burgucheva@gmail.com>
2024-11-19 15:53:51 +0100

    Main page style

--- frontend/src/routes/Index.tsx
+++ frontend/src/routes/Index.tsx
@@ -1,37 +1,24 @@
 // Index page design is from here: https://freefrontend.com/css-image-grids/
 import styles from "./Index.module.css";
+import { baseImgUrl } from "../api";
 
 export default function Index() {
   return (
-    <div className={styles.container}>
-      <div className={styles.wrapper}>
-        <div className={styles.card}>
-          <i className="fal fa-arrow-right"></i>
-        </div>
-      </div>
-      <div className={styles.wrapper}>
-        <div className={styles.card}>
-          <i className="fal fa-arrow-right"></i>
-        </div>
-      </div>
-      <div className={styles.wrapper}>
-        <div className={styles.card}>
-          <i className="fal fa-arrow-right"></i>
-        </div>
-      </div>
-      <div className={styles.wrapper}>
-        <div className={styles.card}>
-          <i className="fal fa-arrow-right"></i>
-        </div>
-      </div>
-      <div className={styles.wrapper}>
-        <div className={styles.card}>
-          <i className="fal fa-arrow-right"></i>
-        </div>
-      </div>
-      <div className={styles.wrapper}>
-        <div className={styles.card}>
-          <i className="fal fa-arrow-right"></i>
+    <div className={styles.item}>
+      <div className={styles.container}>
+        <img
+          className={styles.img}
+          alt="Welcome to my page"
+          src={baseImgUrl + "/mainPageImg.jpg"}
+        />
+        <div className={styles.countryText}>
+          <h1>Welcome to</h1>
+          <h2 className={styles.countryDescription}>my travel blog</h2>
+          <p>
+            Hello everyone! I am Ksenia. I am really keen on traveling and
+            writing down my impressions.
+          </p>
+          <p>Here I would like to share my impressions with you</p>
         </div>
       </div>
     </div>
