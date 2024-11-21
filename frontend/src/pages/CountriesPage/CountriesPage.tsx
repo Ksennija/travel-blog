31437e7069b375f5eceaa66fe5226e70f15e1f66
Ksenia Burgucheva <ksenia.burgucheva@gmail.com>
2024-11-21 15:11:57 +0100

    Add Country panel

--- frontend/src/pages/CountriesPage/CountriesPage.tsx
+++ frontend/src/pages/CountriesPage/CountriesPage.tsx
@@ -7,31 +7,40 @@ import { WelcomePanel } from "./components/WelcomePanel/WelcomePanel";
 import { fetchCountries } from "../../api/countriesApi";
 import { Country } from "../../types/CountryType";
 import styles from "./CountriesPage.module.css";
-import { serialize } from "v8";
-
-export type CountriesProps = {
-  countries: Country[];
-};
 
 export const CountriesPage: React.FC = () => {
   const { countryId } = useParams<CountriesPageParams>();
-  const [countries, setCountries] = useState<Country[]>([]);
+  const [countries, setCountries] = useState<Country[]>();
   const [searchParams] = useSearchParams();
   const query = searchParams.get("q");
+
   useEffect(() => {
     async function fetchAllCountries(query: string | null) {
       const countries = (await fetchCountries(query)) as Country[];
       setCountries(countries);
     }
-
     fetchAllCountries(query);
   }, [query]);
+
+  if (!countries) {
+    return <div>Loading</div>;
+  }
+
+  debugger;
   return (
     <>
       <Sidebar countries={countries} />
       <div className={styles.detail}>
-        {countryId ? <CountryPanel /> : <WelcomePanel />}
+        {countryId ? (
+          <CountryPanel country={getCountry(countries, countryId)} />
+        ) : (
+          <WelcomePanel />
+        )}
       </div>
     </>
   );
 };
+
+function getCountry(countries: Country[], countryId: string) {
+  return countries.find((country) => country.id === countryId)!;
+}
--- frontend/src/pages/CountriesPage/components/CountryPanel/CountryPanel.tsx
+++ frontend/src/pages/CountriesPage/components/CountryPanel/CountryPanel.tsx
@@ -1,4 +1,106 @@
-import React from "react";
+import DOMPurify from "dompurify";
+import { marked } from "marked";
+import { Form, useFetcher } from "react-router-dom";
+import { Country } from "../../types";
+import {
+  baseImgUrl,
+  getCountry,
+  updateCountry,
+} from "../../../../api/countriesApi";
+
+import styles from "./CountryPanel.module.css";
+import React, { useEffect, useRef } from "react";
+
+export type Props = {
+  country: Country;
+};
+// export async function loader({ params }: any) {
+//   const country = await getCountry(params.countryId);
+//   if (!country) {
+//     throw new Response("", {
+//       status: 404,
+//       statusText: "Not Found",
+//     });
+//   }
+//   return { country };
+// }
+
+// export async function action({ request, params }: any) {
+//   const formData = await request.formData();
+//   return updateCountry(params.countryId, {
+//     favourite: formData.get("favourite") === "true",
+//   });
+// }
+
+export const CountryPanel: React.FC<Props> = ({ country }: Props) => {
+  debugger;
+  const descriptionElRef = useRef<HTMLParagraphElement>(null);
+
+  useEffect(() => {
+    if (descriptionElRef.current) {
+      descriptionElRef.current.innerHTML = DOMPurify.sanitize(
+        marked.parse(country.description) as string
+      );
+    }
+  }, [country.description]);
+
+  return (
+    <div key={country.id} className={styles.countryItem}>
+      <div className={styles.buttonPanel}>
+        <form action="edit">
+          <button>Edit</button>
+        </form>
+        <form
+          method="post"
+          action="destroy"
+          //   onSubmit={(event) => {
+          //     if (
+          //       !window.confirm("Please confirm you want to delete this record.")
+          //     ) {
+          //       event.preventDefault();
+          //     }
+          //   }}
+        >
+          <button>Delete</button>
+        </form>
+      </div>
+      <div className={styles.countryContainer}>
+        <img
+          className={styles.countryImg}
+          alt={country.name}
+          src={country.imageUrl && baseImgUrl + country.imageUrl}
+        />
+        <div className={styles.countryText}>
+          <h1>
+            {country.name}
+            {/* <Favourite country={country} /> */}
+          </h1>
+          <p className={styles.countryDescription} ref={descriptionElRef} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+function Favourite({ country }: Props) {
+  //const fetcher = useFetcher();
+  //   const favourite = fetcher.formData
+  //     ? fetcher.formData.get("favourite") === "true"
+  //     : country.favourite;
+  //   return (
+  //     <form method="post">
+  //       <button
+  //         name="favourite"
+  //     //     value={favourite ? "false" : "true"}
+  //     //     aria-label={favourite ? "Remove from favourites" : "Add to favourites"}
+  //     //   >
+  //     //     {favourite ? "★" : "☆"}
+  //       </button>
+  //     </form>
+  //   );
+}
+
+/* import React from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { CountriesPageParams } from "../../types";
 import styles from "./CountryPanel.module.css";
@@ -20,4 +122,4 @@ export const CountryPanel: React.FC = () => {
       </button>
     </div>
   );
-};
+}; */
